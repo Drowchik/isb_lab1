e0a6557233426f39a17e2a0b4546b9a046e1736b
natal <natal@CLOWN>
2024-04-10 17:33:27 +0400

    fix_complete_try2

--- lab_1/task1/utils.py
+++ lab_1/task1/utils.py
@@ -1,5 +1,13 @@
-from enum import Enum
 import json
+import os
+import sys
+from enum import Enum
+
+project_directory = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_directory)
+
+from general_function import read_file, write_file
 
 ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
@@ -22,39 +30,6 @@ def main(file_name: str, mode: Mode, res_nume: str, path_js: str, shift: int = 3
     text = encryption(res, mode, shift)
     write_file(text, res_nume)
     create_json(path_js, shift)
-
-
-def read_file(name: str) -> str:
-    """ 
-        This function reads text from the file for further processing
-
-    Args:
-        name (str): the path and name of the file to be read
-
-    Returns:
-        str: the read document
-    """
-    try:
-        with open(name, 'r', encoding='utf-8') as f:
-            data = f.read()
-            return data
-    except FileNotFoundError:
-        print("Невозможно открыть файл")
-
-
-def write_file(document: str, name_file: str) -> None:
-    """
-        Writes encrypted text to a document 
-    Args:
-        document (str): the actual processed text
-        name_file (_type_): the path of the file where it will be written
-    """
-    try:
-        with open(name_file, "w", encoding='utf-8') as f:
-            f.write(document)
-            return
-    except IOError:
-        print("Что-то пошло не так")
 
 
 def encryption(document: str, mode: Enum, shift: int) -> str:
