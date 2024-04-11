abfda4f293bdd32f0928e0b0fd5122ab4ed69441
natal <natal@CLOWN>
2024-04-11 20:13:13 +0400

    fixed the frequency

--- lab_1/task2/utils.py
+++ lab_1/task2/utils.py
@@ -20,12 +20,18 @@ def auxiliary_function(text: str) -> None:
         text (str): document for encryption
     """
     info_words = Counter(text)
-    print(f"Информация по частоте встречи символов: {info_words}")
+    count_char = len(text)
+    dict_words = dict()
+    for char, count in info_words.items():
+        dict_words[char] = count / count_char
+    dict_words = {key: count for key, count in sorted(
+        dict_words.items(), key=lambda item: item[1], reverse=True)}
+    print(f"-Информация по частоте встречи символов: {dict_words}")
     words_in_text = text.split(info_words.most_common(1)[0][0])
-    print(f"Список слов разделённых пробелами: {words_in_text}")
+    print(f"-Список слов разделённых пробелами: {words_in_text}")
     words_in_text = sorted(words_in_text, key=lambda x: len(x))
     print(
-        f"Список отсортированных по длине слов разделённых пробелами: {words_in_text}")
+        f"-Список отсортированных по длине слов разделённых пробелами: {words_in_text}")
 
 
 def decryption(path_res: str, path: str, text: str) -> str:
@@ -52,6 +58,6 @@ def decryption(path_res: str, path: str, text: str) -> str:
 if __name__ == "__main__":
     path = read_json_dict(path_js)
     text = read_file(path["cod2"])
-    text = decryption(path["res_text2"], path["key_for_code"], text)
-    write_file(text, path["res_text2"])
-
+    auxiliary_function(text)
+    # text = decryption(path["res_text2"], path["key_for_code"], text)
+    # write_file(text, path["res_text2"])
